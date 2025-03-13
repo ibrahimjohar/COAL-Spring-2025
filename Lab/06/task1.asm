1f0e55bbd1c4cc7e89b4952aad4ea5d5f6cdd242
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-03-13 12:17:05 +0500

    Create task1.asm

---
+++ Lab/06/task1.asm
@@ -0,0 +1,16 @@
+include irvine32.inc
+.code
+main PROC
+    mov eax, 0          
+    mov ebx, 1          
+    mov ecx, 10         
+
+L1:
+    call WriteDec       
+    call Crlf           
+    add eax, ebx        
+    xchg eax, ebx       
+    loop L1             
+    exit
+main ENDP
+END main
