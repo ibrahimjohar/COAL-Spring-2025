0135f2affea0eaa9cd5d8f54428292f01e4b0dce
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-03-19 01:17:53 +0500

    Create sum-of-array-loop.asm

---
+++ Notes/sum-of-array-loop.asm
@@ -0,0 +1,27 @@
+;Write a loop that iterates through a doubleword array and 
+;calculates the sum of its elements using a scale factor with
+;indexed addressing.
+
+include irvine32.inc
+.data
+	arrayD DWORD 10,20,30,40,50	;doubleword array
+	sum DWORD 0
+.code
+main PROC
+	mov esi, OFFSET	arrayD		;load base address of array
+	mov ecx, LENGTHOF arrayD	;set loop counter (num of elements in array)
+	mov eax, 0					;initialise sum to 0
+L1:
+	add eax, [esi]				;add current element to sum
+	add esi, TYPE arrayD		;move to next element(DWORD = 4 bytes)
+	loop L1
+
+	mov sum, eax				;store the final sum
+
+	exit
+	main ENDP
+END main
+
+; Output
+; EAX = 150
+; sum = 150
