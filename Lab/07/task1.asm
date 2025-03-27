33d5c98b7c81d2ade7e725ba9a8650c1ae1b9d8b
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-03-27 13:02:26 +0500

    Update task1.asm

--- Lab/07/task1.asm
+++ Lab/07/task1.asm
@@ -1,30 +1,37 @@
 include irvine32.inc
 .data
-    source WORD 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
-    destination WORD 10 DUP(0)
+    source DWORD 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+    destination DWORD 10 DUP(0)
+    index DWORD 0
 .code
 main PROC
-    lea esi, source
-    lea edi, destination
+    ;push all source elements to stack
     mov ecx, 10
+    mov esi, OFFSET source
+L1:
+    mov eax, [esi]
+    push eax
+    add esi, 4
+    loop L1
 
-copy_loop:
-    push WORD PTR [esi]
-    add esi, 2
-    pop WORD PTR [edi]
-    add edi, 2
-    loop copy_loop
-
-    ;display
-    lea edi, destination
+    ;pop them into destination array
     mov ecx, 10
+    mov edi, OFFSET destination
+L2:
+    pop eax
+    mov [edi], eax
+    add edi, 4
+    loop L2
 
-print_loop:
-    movzx eax, WORD PTR [edi]
+    ;display destination array
+    mov ecx, 10
+    mov esi, OFFSET destination
+L3:
+    mov eax, [esi]
     call WriteDec
     call Crlf
-    add edi, 2
-    loop print_loop
+    add esi, 4
+    loop L3
 
     exit
 main ENDP
