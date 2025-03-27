bc954f46d3d2a1085c18ef7506c8c1300f3313d3
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-03-28 01:21:15 +0500

    Update task3.asm

--- Lab/07/task3.asm
+++ Lab/07/task3.asm
@@ -1,58 +1,66 @@
 include irvine32.inc
 .data
-    arr1 WORD 1, 2, 3, 4, 5
-    arr2 WORD 6, 7, 8, 9, 10
+    array1 DWORD 1, 2, 3, 4, 5
+    array2 DWORD 6, 7, 8, 9, 10
     sum1 DWORD ?
     sum2 DWORD ?
     total DWORD ?
+    totalMsg BYTE "total sum of both arrays is: ", 0
 .code
-sumArray1 PROC
-    lea esi, arr1
-    mov ecx, 5
-    xor eax, eax
-
-L1:
-    add ax, [esi]
-    add esi, 2
-    loop L1
-
-    movzx eax, ax
-    mov sum1, eax
-    ret
-sumArray1 ENDP
-
-sumArray2 PROC
-    lea esi, arr2
-    mov ecx, 5
-    xor eax, eax
-
-L2:
-    add ax, [esi]
-    add esi, 2
-    loop L2
-
-    movzx eax, ax
-    mov sum2, eax
-    ret
-sumArray2 ENDP
-
-addBoth PROC
-    mov eax, sum1
-    add eax, sum2
-    mov total, eax
-    ret
-addBoth ENDP
-
 main PROC
-    call sumArray1
-    call sumArray2
-    call addBoth
+    call SumArray1
+    call SumArray2
+    call SumBoth
 
-    ;total display
+    ;result
+    mov eax, total
+    call WriteString
+    mov edx, OFFSET totalMsg
+    call WriteString
     mov eax, total
     call WriteDec
     call Crlf
 
     exit
 main ENDP
+
+; ------------------------------------------
+
+SumArray1 PROC
+    mov ecx, 5
+    mov esi, OFFSET array1
+    xor eax, eax     ;clearing eax to accumulate sum
+L1:
+    add eax, [esi]
+    add esi, 4
+    loop L1
+    mov sum1, eax
+    ret
+SumArray1 ENDP
+
+; ------------------------------------------
+
+
+SumArray2 PROC
+    mov ecx, 5
+    mov esi, OFFSET array2
+    xor eax, eax
+L2:
+    add eax, [esi]
+    add esi, 4
+    loop L2
+    mov sum2, eax
+    ret
+SumArray2 ENDP
+
+; ------------------------------------------
+
+
+SumBoth PROC
+    mov eax, sum1
+    add eax, sum2
+    mov total, eax
+    ret
+SumBoth ENDP
+
 END main
