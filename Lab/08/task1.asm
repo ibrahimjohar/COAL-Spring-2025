98c33ab567ed5dfdb57d436971d7ec3d879983d5
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-04-28 00:12:31 +0500

    Update task1.asm

--- Lab/08/task1.asm
+++ Lab/08/task1.asm
@@ -1,36 +1,41 @@
 include irvine32.inc
 .data
-  arr DWORD 4 DUP (?)
-  msg2 BYTE "all integers are equal",0
-  msg3 BYTE "integers are not equal",0
-  msg1 BYTE "enter 4 integers: ",0
-
+	arr DWORD 4 DUP (?)
+	msg1 BYTE "enter 4 integers: ",0
+	msg2 BYTE "all integers are equal",0
+	msg3 BYTE "integers are not equal",0
 .code
 main PROC
-  mov edx, OFFSET msg1
-  call WriteString
-  mov ecx, 4
-  mov esi, 0
+	mov edx, OFFSET msg1
+	call WriteString
+	mov ecx, 4
+	mov esi, 0
+
 L1:
-  call ReadInt
-  mov arr[esi],eax
-  add esi, 4
-  loop L1
-  mov esi,0
-  mov ecx, 4
-  mov eax, arr[esi]
-Compare:
-  cmp eax,arr[esi]
-  JNE endstat
-  add esi,4
-  Loop Compare
-  mov edx, OFFSET msg2
-  call WriteString
-  jmp callend
+	call ReadInt
+	mov arr[esi], eax
+	add esi, 4
+	loop L1
+	
+	mov esi, 0
+	mov ecx, 4
+	mov eax, arr[esi]
+
+compare:
+	cmp eax, arr[esi]
+	JNE endstat
+	add esi, 4
+	loop compare
+	
+	mov edx, OFFSET msg2
+	call WriteString
+	jmp callend
+
 endstat:
-  mov edx, OFFSET msg3
-  call WriteString
+	mov edx, OFFSET msg3
+	call WriteString
+	
 callend:
-  exit
+	exit
 main ENDP
 END main
