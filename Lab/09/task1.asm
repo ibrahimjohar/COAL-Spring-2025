35729a177151a4ac322b0f93cce39a9ee579642b
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-05-08 05:35:55 +0500

    Update task1.asm

--- Lab/09/task1.asm
+++ Lab/09/task1.asm
@@ -1,1 +1,28 @@
+include irvine32.inc
+.data
+	var1 DWORD 2
+    var2 DWORD 3
+    var3 DWORD 4
+.code
+main PROC
+	push var3
+	push var2
+	push var1
+	call ThreeProd
+	add esp, 12			;clean up the 3 arguments (3*4bytes)
+	call WriteDec
+	exit
+main ENDP
 
+
+ThreeProd PROC
+	push ebp
+	mov ebp, esp
+	mov eax, [ebp+8]	;var1
+	mul DWORD PTR [ebp+12]		;var2
+	mul DWORD PTR [ebp+16]		;var3
+	pop ebp
+	ret
+ThreeProd ENDP
+
+END main
