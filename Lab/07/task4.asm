fd4b4b9aa0d9bea5720fbb3e6cd515ac4237cdc1
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-05-10 14:33:17 +0500

    Update task4.asm

--- Lab/07/task4.asm
+++ Lab/07/task4.asm
@@ -1,36 +1,43 @@
 include irvine32.inc
 .data
-    columns DWORD 5     ;number of columns
-    star BYTE '*'
+	star BYTE "*", 0
+	space BYTE " ", 0
+	input BYTE "enter num of columns: ", 0
+	col DWORD ?
 .code
 main PROC
-    mov eax, columns    
-    call print_pattern
-
-    exit
+	mov edx, OFFSET input
+	call writestring
+	call readdec
+	mov col, eax
+	call crlf
+	call patternPrint
+	call crlf
+	exit
 main ENDP
 
-;-------------------------------------------------
+patternPrint PROC
+	mov ecx, col
+	mov eax, 1
+L1:
+	push ecx
+	mov ebx, ecx
+	mov ecx, ebx
+L2:
+	mov edx, OFFSET space
+	call writestring
+	loop L2
+	mov ecx, eax
+L3:
+	mov edx, OFFSET star
+	call writestring
+	loop L3
+	inc eax
+	call crlf
+	pop ecx
+	loop L1
+	ret
+patternPrint ENDP
 
-print_pattern PROC
-    ;eax = num of columns
-    mov esi, eax        ;save cols value in esi
-    mov ebx, 1          ;row counter
-
-outer_loop:
-    mov ecx, ebx        ;num of stars in curr row
-
-inner_loop:
-    mov al, star
-    call WriteChar
-    loop inner_loop
-
-    call Crlf
-
-    inc ebx
-    cmp ebx, esi
-    jle outer_loop     ;looping till row = cols
-    ret
-print_pattern ENDP
-
+	exit
 END main
