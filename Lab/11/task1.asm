6e836455330a8589e7d382b8c72aa77eaf2ecce3
Ibrahim Johar Farooqi <34939623+ibrahimjohar@users.noreply.github.com>
2025-05-10 20:19:53 +0500

    Update task1.asm

--- Lab/11/task1.asm
+++ Lab/11/task1.asm
@@ -1,1 +1,21 @@
+include irvine32.inc
+.data
+    msg0 byte "final result: ", 0
+.code
+main PROC
+    mov eax, 5      
+    mov ebx, eax      
+    mov ecx, eax    
+    shl eax, 4        
+    shl ebx, 2        
+    add eax, ebx      
+    add eax, ecx      
 
+    mov edx, OFFSET msg0
+    call WriteString   
+    call WriteDec      
+    call Crlf         
+    
+    exit
+main ENDP
+END main
